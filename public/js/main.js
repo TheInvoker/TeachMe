0271f4475bb8d01d2c3d57886ff98e10c332d0b9
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-04-17 15:23:53 -0400

    refactored code

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 var LOC_LIST = [];
 var SB_LIST = [];
+var CUR_LOCATION = null;
 
 function addMarker(map, cluster, marker, content, markerCB) {
 	map.marker(marker).then(function(marker) {
@@ -72,92 +73,97 @@ function cluster(map, class1, class2, class3, clusterCallback) {
 	}).then(clusterCallback);
 }
 
+function updatePosition(position) {
+	CUR_LOCATION = position;
+}
 
+function updateLocationsOnCluster(map, cluster) {
+	for(var i=0; i<LOC_LIST.length; i+=1) {
+		cluster.remove(LOC_LIST[i]);
+		LOC_LIST[i].setMap(null);
+	}
+	LOC_LIST = [];
+	
+	getLocations(CUR_LOCATION, function(data) {
+		if (data.status == 'ok') {
+			var array = data.data;
+			for(var i=0; i<array.length; i+=1) {
+				addMarker(map, cluster, {
+					position: array[i].pos,
+					clickable : false,
+					draggable : false
+				}, "<div class='infowindow " + array[i].type + "'>" + array[i].text + "</div>", function(marker) {
+					LOC_LIST.push(marker);
+				});
+			}
+		}
+	});
+}
+
+function updateStoresOnCluster(map, cluster) {
+	for(var i=0; i<SB_LIST.length; i+=1) {
+		cluster.remove(SB_LIST[i]);
+		SB_LIST[i].setMap(null);
+	}
+	SB_LIST = [];
+	
+	$.ajax({
+		type: 'GET',
+		url: 'https://openapi.starbucks.com/location/v1/stores?radius=10&limit=50&brandCode=SBUX&latLng=' + CUR_LOCATION.lat + '%2C' + CUR_LOCATION.lng + '&apikey=7b35m595vccu6spuuzu2rjh4',
+		dataType: 'json',
+		headers: {
+			'Accept': 'application/json'
+		},
+		success: function(storesData) {
+			var array = storesData.items;
+			for(var i=0; i<array.length; i+=1) {
+				var pos = array[i].store.coordinates;
+				addMarker(map, cluster, {
+					position: {'lat' : pos.latitude, 'lng':pos.longitude},
+					icon : "image/starbucks.png",
+					clickable : false,
+					draggable : false
+				}, null, function(marker) {
+					SB_LIST.push(marker);
+				});
+			}
+		},
+		error:function(jqXHR, textStatus, errorThrown){
+			alert("error");
+		}
+	});
+}
 
 function initMap() {
-
+	
+	setInterval(function() {
+		getCurrentLocation(updatePosition);
+	}, 1000);
+	
 	getCurrentLocation(function(position) {
+		updatePosition(position);
 		
 		var map = $('#map')
 		  .gmap3({
 			zoom: 13,
 			center: position
 		  });
-		  
-		
-		var a = function(cluster) {
-			for(var i=0; i<LOC_LIST.length; i+=1) {
-				cluster.remove(LOC_LIST[i]);
-				LOC_LIST[i].setMap(null);
-			}
-			LOC_LIST = [];
-			
-			getCurrentLocation(function(position) {
-				getLocations(position, function(data) {
-					if (data.status == 'ok') {
-						var array = data.data;
-						for(var i=0; i<array.length; i+=1) {
-							addMarker(map, cluster, {
-								position: array[i].pos,
-								clickable : false,
-								draggable : false
-							}, "<div class='infowindow " + array[i].type + "'>" + array[i].text + "</div>", function(marker) {
-								LOC_LIST.push(marker);
-							});
-						}
-					}
-				});
-			});
-		};
+
 		cluster(map, "cluster-1", "cluster-2", "cluster-3", function(cluster) {
-			a(cluster);
-			setInterval(a, 60 * 1000, cluster);
+			updateLocationsOnCluster(map, cluster);
+			setInterval(updateLocationsOnCluster, 60 * 1000, map, cluster);
 		});
 
-		var b = function(cluster) {
-			for(var i=0; i<SB_LIST.length; i+=1) {
-				cluster.remove(SB_LIST[i]);
-				SB_LIST[i].setMap(null);
-			}
-			SB_LIST = [];
-			
-			$.ajax({
-				type: 'GET',
-				url: 'https://openapi.starbucks.com/location/v1/stores?radius=10&limit=50&brandCode=SBUX&latLng=' + position.lat + '%2C' + position.lng + '&apikey=7b35m595vccu6spuuzu2rjh4',
-				dataType: 'json',
-				headers: {
-					'Accept': 'application/json'
-				},
-				success: function(storesData) {
-					var array = storesData.items;
-					for(var i=0; i<array.length; i+=1) {
-						var pos = array[i].store.coordinates;
-						addMarker(map, cluster, {
-							position: {'lat' : pos.latitude, 'lng':pos.longitude},
-							icon : "image/starbucks.png",
-							clickable : false,
-							draggable : false
-						}, null, function(marker) {
-							SB_LIST.push(marker);
-						});
-					}
-				},
-				error:function(jqXHR, textStatus, errorThrown){
-					alert("error");
-				}
-			});
-		};
 		cluster(map, "cluster-1", "cluster-2", "cluster-3", function(cluster) {
-			b(cluster);
-			setInterval(b, 60 * 1000, cluster);
+			updateStoresOnCluster(map, cluster);
+			setInterval(updateStoresOnCluster, 60 * 1000, map, cluster);
 		});
-
-		setInterval(function() {
-			setInfoWindow("request");
-			setInfoWindow("teach");
-		}, 100);
-	
 	});
+	
+	setInterval(function() {
+		setInfoWindow("request");
+		setInfoWindow("teach");
+	}, 100);
 }
 
 function getLocations(position, callback) {
