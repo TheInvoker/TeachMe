40e14d4f59d5d563f8e412a8ab3b834f2c406dfa
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-05-01 17:48:56 -0400

    fi

--- server.js
+++ server.js
@@ -17,53 +17,51 @@ app.get('/', function (req, res) {
 
 app.post('/getlocations', function (req, res) {
 	
-    if (req.method == 'POST') {
-        var body = '';
+	var body = '';
 
-        req.on('data', function (data) {
-            body += data;
+	req.on('data', function (data) {
+		body += data;
 
-            // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6)
-                req.connection.destroy();
-        });
+		// Too much POST data, kill the connection!
+		// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+		if (body.length > 1e6)
+			req.connection.destroy();
+	});
 
-        req.on('end', function () {
-            var post = qs.parse(body);
-			var lat = parseFloat(post.lat);
-			var lng = parseFloat(post.lng);
-			var mi = -0.01, ma = 0.01;
-			
-			var dataList = [
-				{
-					'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
-					'type':'request',
-					'text':'I want to learn how to do a cool coin magic trick'
-				},
-				{
-					'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
-					'type':'teach',
-					'text':'I can teach you to meditate'
-				},
-				{
-					'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
-					'type':'request',
-					'text':'I want to learn how tie a tie'
-				},
-				{
-					'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
-					'type':'teach',
-					'text':'I can help you with your resume'
-				}
-			];
-			
-			res.end(JSON.stringify({
-				'status':'ok',
-				'data': dataList
-			}));
-        });
-    }
+	req.on('end', function () {
+		var post = qs.parse(body);
+		var lat = parseFloat(post.lat);
+		var lng = parseFloat(post.lng);
+		var mi = -0.01, ma = 0.01;
+		
+		var dataList = [
+			{
+				'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
+				'type':'request',
+				'text':'I want to learn how to do a cool coin magic trick'
+			},
+			{
+				'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
+				'type':'teach',
+				'text':'I can teach you to meditate'
+			},
+			{
+				'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
+				'type':'request',
+				'text':'I want to learn how tie a tie'
+			},
+			{
+				'pos':{lat: lat+random(mi,ma), lng: lng+random(mi,ma)},
+				'type':'teach',
+				'text':'I can help you with your resume'
+			}
+		];
+		
+		res.end(JSON.stringify({
+			'status':'ok',
+			'data': dataList
+		}));
+	});
 });
 
 var server = app.listen(3000, function () {
